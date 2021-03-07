7ea71b680bba87df957d6d1fc91710d9541ef497
simon <tran.simon2@gmail.com>
2021-03-06 22:35:44 -0500

    added windows scripts

---
+++ windows-backend.sh
@@ -0,0 +1,2 @@
+docker build -t inf3995-backend backend && docker run -it -p 5000:5000 -v './backend':'/root/backend' inf3995-backend
+
---
+++ windows-simulation.sh
@@ -0,0 +1,2 @@
+
+x11docker --user=RETAIN --runx -- -p 80:80 -w /root/simulation  -- inf3995-simulation  /root/simulation/test.sh
