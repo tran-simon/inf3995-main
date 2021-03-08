011ec9af95210e7e7ba62498bf7e9677c8f002d2
simon <tran.simon2@gmail.com>
2021-03-08 00:07:38 -0500

    CDR

--- windows-simulation.sh
+++ windows-simulation.sh
@@ -1,2 +1,2 @@
 
-x11docker --user=RETAIN --runx -- -p 80:80 -w /root/simulation  -- inf3995-simulation  /root/simulation/test.sh
+x11docker --user=RETAIN --runx -- -p 80:80 -w /root/simulation  -- inf3995-simulation  /root/simulation/windows-run.sh
