b0179a06a03eb5b706def78ad0aa5ae0c665c0e5
simon <tran.simon2@gmail.com>
2021-04-15 17:08:34 -0400

    Merged working versions to master

--- run-simulation.sh
+++ run-simulation.sh
@@ -1,2 +1,2 @@
 
-x11docker --user=RETAIN --runx --workdir=/root/simulation -- -p 80:80 -- inf3995-simulation argos3 -c ./experiments/crazyflie_sensing.argos
+x11docker --user=RETAIN --workdir=/root/simulation -- -- inf3995-simulation argos3 -c ./experiments/crazyflie_sensing.argos
