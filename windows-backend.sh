e6009df870a56e7968e95c4c807b818726308c9a
simon <tran.simon2@gmail.com>
2021-04-17 16:38:05 -0400

    Ãremoved old scripts

--- windows-backend.sh
+++
@@ -1,2 +0,0 @@
-docker build -t inf3995-backend backend && docker run -it -p 5000:5000 -v './backend':'/root/backend' inf3995-backend
-
--- windows-simulation.sh
+++
@@ -1,2 +0,0 @@
-
-x11docker --user=RETAIN --runx --workdir=/root/simulation -- -p 80:80 -w /root/simulation  -- inf3995-simulation  argos3 -c ./experiments/crazyflie_sensing.argos
